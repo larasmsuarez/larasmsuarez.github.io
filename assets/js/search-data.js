a2c7f1031f69f7699404a1cb030d51017cfe0d31
larasmsuarez <larasanmartin1@gmail.com>
2026-06-20 00:54:28 +0000

    Deploying to gh-pages from @ larasmsuarez/larasmsuarez.github.io@701caa79cef76fd46c3426c0909c978fa83f6b9c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/activities/";
           },
+        },{id: "nav-articles",
+          title: "Articles",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
         },{id: "nav-projects",
           title: "Projects",
           description: "",
